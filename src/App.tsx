3a31cbed2f5fb28280dd2661419b859b0c521759
dev5 <dev5@baunfire.com>
2023-08-12 18:29:26 +0700

    additional dialog for limit working hours

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import {Provider as ReduxProvider} from 'react-redux'
-import './App.css';
-import Search from './pages/Search';
-import {store} from './providers';
-
+import React, { useState, useEffect } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import "./App.css";
+import Search from "./pages/Search";
+import { store } from "./providers";
+import BlockerModal from "./components/Modal/BlockerModal";
 
 function App() {
+  const [isBlocked, setIsBlocked] = useState(false);
+
+  useEffect(() => {
+    const currentTime = new Date();
+    const dateTest = new Date("2023-08-12T01:11:10+07:00");
+    const isTest = true;
+    const hours = (isTest ? dateTest : currentTime)
+      .getHours()
+      .toString()
+      .padStart(2, "0");
+
+    setIsBlocked(Number(hours) < 6 || Number(hours) > 23);
+
+  }, []);
+
   return (
-  <ReduxProvider store={store}>
-    <Search/>
-  </ReduxProvider>
+    <ReduxProvider store={store}>
+      <BlockerModal isShowModal={isBlocked} />
+      <Search isBlocked={isBlocked} />
+    </ReduxProvider>
   );
 }
 
---
+++ src/components/Modal/BlockerModal.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Typography from "@mui/material/Typography";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "80vw",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 4,
+};
+
+interface BlockerModalProps {
+  isShowModal: boolean;
+}
+
+export default function BlockerModal({ isShowModal }: BlockerModalProps) {
+  const [open, setOpen] = useState(isShowModal);
+
+  useEffect(() => {
+    setOpen(isShowModal)
+  }, [isShowModal])
+  
+
+  const handleClose = (
+    event: {},
+    reason: "backdropClick" | "escapeKeyDown"
+  ) => {
+    if (reason && reason === "backdropClick") return;
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Thông báo
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Hiện tại hệ thống đang được bảo trì.<br></br>
+              Xin quý phụ huynh quay lại sau <strong>6h sáng</strong>.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
